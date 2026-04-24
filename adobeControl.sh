c0b776f47c9515134b173cfbfe988440f6e74626
therobbie <rhender07@gmail.com>
2026-04-23 20:53:58 -0400

    Update adobeControl.sh
    Fixed parsing function. Converted to zsh.

--- adobeControl.sh
+++ adobeControl.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 # Author: Robert Henderson
-# Date: 03-20-2026
+# Date: 04-23-2026
 #
-## Adobe Control Script version 3.0
+## Adobe Control Script version 3.2
+## Converted to zsh
 ##
-
 ## Updated handling to compute the base version. Using adobeuninstaller --list output to get this value.
 
 ## Description:
 #   Script will handle uninstalling Adobe Applications and also reinstalling based on what is "called".
-#   Control will be handle from JAMF Pro using scripti variables.
+#   Control will be handled from JAMF Pro using script variables.
 ## Variable 4: Action variable (i.e. Uninstall or Install)
 ## Variable 5: Year Version (i.e. 2024 or 2025 etc..)
 ## Variable 6: All or Specific Suites (i.e. Photo, Video, Design, All)
@@ -17,7 +17,8 @@
 #### Variables ####
 actionVar="${4}"
 appYear="${5}"
-appList=($6 $7 $8 $9 ${10} ${11})
+# Create an array of the JAMF variables. Zsh handles unquoted variables safely here.
+appList=(${=6} ${=7} ${=8} ${=9} ${=10} ${=11})
 
 appPath=""      # Holds path of app we are working on
 appSAPcode=""   # Holds Adobe SAP Code for application  
@@ -27,102 +28,131 @@ appBaseVersion="" # Holds base version for app selected
 
 #### FUNCTIONS ####
 
+# Usage: find_base <SapCode> <Version>
+find_base() {
+    local target_sap=$1
+    local target_ver=$2
+
+    # Loop through the DATA string line-by-line
+    # read -rA splits each line by whitespace into an array called 'words'
+    while read -rA words; do
+        # Skip empty lines or header lines (lines with less than 3 words, or starting with Name/---)
+        if [[ ${#words[@]} -lt 3 || "${words[1]}" == "Name" || "${words[1]}" == \-* ]]; then
+            continue
+        fi
+
+        # Zsh supports negative indexing to grab items from the end of an array
+        local row_ver=${words[-1]}
+        local row_base=${words[-2]}
+        local row_sap=${words[-3]}
+
+        # Check for a match
+        if [[ "$row_sap" == "$target_sap" && "$row_ver" == "$target_ver"* ]]; then
+            echo "$row_base"
+            return 0 # Exit function successfully
+        fi
+    done <<< "$adobeUninstallerInfo"
+
+    # If the loop finishes without returning, the item wasn't found
+    echo "Not Found"
+}
+
 appSelector() {
     local appSelected="${1}"
 
     case ${appSelected} in
         "acrobat")
-            echo "App choosen: acrobat"
+            echo "App chosen: acrobat"
             appPath="/Applications/Adobe Acrobat DC/Adobe Acrobat.app"
             appSAPcode="APRO"
             appEventID="rit_adobeacrobatsdl"
             ;;
         "aftereffects")
-            echo "App choosen: after effects"
+            echo "App chosen: after effects"
             appPath="/Applications/Adobe After Effects ${appYear}/Adobe After Effects ${appYear}.app"
             appSAPcode="AEFT"
             appEventID="rit_adobeaftereffectssdl"
             ;;
         "animate")
-            echo "App choosen: animate"
+            echo "App chosen: animate"
             appPath="/Applications/Adobe Animate 2024/Adobe Animate 2024.app"
             appSAPcode="FLPR"
             appEventID="rit_adobeanimatesdl"
             ;;    
         "audition")
-            echo "App choosen: audition"
+            echo "App chosen: audition"
             appPath="/Applications/Adobe Audition ${appYear}/Adobe Audition ${appYear}.app"
             appSAPcode="AUDT"
             appEventID="rit_adobeauditionsdl"
             ;;
         "bridge")
-            echo "App choosen: bridge"
+            echo "App chosen: bridge"
             appPath="/Applications/Adobe Bridge ${appYear}/Adobe Bridge ${appYear}.app"
             appSAPcode="KBRG"
             appEventID="rit_adobebridgesdl"
             ;;
         "character")
-            echo "App choosen: character"
+            echo "App chosen: character"
             appPath="/Applications/Adobe Character Animator ${appYear}/Adobe Character Animator ${appYear}.app"
             appSAPcode="CHAR"
             appEventID="rit_adobecharacteranimator"
             ;;
         "dimension")
-            echo "App choosen: dimension"
+            echo "App chosen: dimension"
             appPath="/Applications/Adobe Dimension/Adobe Dimension.app"
             appSAPcode="ESHR"
             appEventID="rit_adobedimension"
             ;;
         "dreamweaver")
-            echo "App choosen: dreamweaver"
+            echo "App chosen: dreamweaver"
             appPath="/Applications/Adobe Dreamweaver 2021/Adobe Dreamweaver 2021.app"
             appSAPcode="DRWV"
             appEventID="rit_adobedreamweaversdl"
             ;;
         "illustrator")
-            echo "App choosen: illustrator"
+            echo "App chosen: illustrator"
             appPath="/Applications/Adobe Illustrator ${appYear}/Adobe Illustrator.app"
             appSAPcode="ILST"
             appEventID="rit_adobeillustratorsdl"
             ;;
         "incopy")
-            echo "App choosen: incopy"
+            echo "App chosen: incopy"
             appPath="/Applications/Adobe InCopy ${appYear}/Adobe InCopy ${appYear}.app"
             appSAPcode="AICY"
             appEventID="rit_adobeincopysdl"
             ;;
         "indesign")
-            echo "App choosen: indesign"
+            echo "App chosen: indesign"
             appPath="/Applications/Adobe InDesign ${appYear}/Adobe InDesign ${appYear}.app"
             appSAPcode="IDSN"
             appEventID="rit_adobeindesignsdl"
             ;;
         "lightroomclassic")
-            echo "App choosen: lightroomclassic"
+            echo "App chosen: lightroomclassic"
             appPath="/Applications/Adobe Lightroom Classic/Adobe Lightroom Classic.app"
             appSAPcode="LTRM"
             appEventID="rit_adobelightroomclassicsdl"
             ;;
         "mediaencoder")
-            echo "App choosen: media encoder"
+            echo "App chosen: media encoder"
             appPath="/Applications/Adobe Media Encoder ${appYear}/Adobe Media Encoder ${appYear}.app"
             appSAPcode="AME"
             appEventID="rit_adobemediaencoder"
             ;;
         "photoshop")
-            echo "App choosen: photoshop"
+            echo "App chosen: photoshop"
             appPath="/Applications/Adobe Photoshop ${appYear}/Adobe Photoshop ${appYear}.app"
             appSAPcode="PHSP"
             appEventID="rit_adobephotoshopsdl"
             ;;
         "premierepro")
-            echo "App choosen: premiere pro"
+            echo "App chosen: premiere pro"
             appPath="/Applications/Adobe Premiere Pro ${appYear}/Adobe Premiere Pro ${appYear}.app"
             appSAPcode="PPRO"
             appEventID="rit_adobepremiereprosdl"
             ;;
         "premiererush")
-            echo "App choosen: premiere rush"
+            echo "App chosen: premiere rush"
             appPath="/Applications/Adobe Premiere Rush 2.0/Adobe Premiere Rush.app"
             appSAPcode="RUSH"
             appEventID="rit_adobepremiererushsdl"
@@ -131,12 +161,11 @@ appSelector() {
             echo "App selected was not found in selector."
             ;;
     esac
-
 }
 
 appUninstall() {
     # Check to see if the Application is still present
-    if [ -d "${appPath}" ]; then
+    if [[ -d "${appPath}" ]]; then
         
         echo ""
         echo "Product Key: ${appSAPcode}"
@@ -152,61 +181,54 @@ appUninstall() {
         adobeUninstallerInfo=$(/usr/local/adobe/AdobeUninstaller --list)
 
         # Extract only the major and minor numbers (xx.xx) from the user's input
-        # If you input 26.11.4, this converts it to 26.11
         MAJOR_MINOR=$(echo "$appVersion" | sed -E 's/^([0-9]+\.[0-9]+).*$/\1/')
 
-        # Parse the variable using macOS-compatible BSD sed and a Here String (<<<) using data stored from uninstaller command
-        appBaseVersion=$(sed -n -E "s/^.*[[:space:]]${TARGET_SAPCODE}[[:space:]]+([^[:space:]]+)[[:space:]]+${MAJOR_MINOR}(\.[^[:space:]]+)?[[:space:]]*$/\1/p" <<< "${adobeUninstallerInfo}")
+        appBaseVersion=$(find_base "${appSAPcode}" "${MAJOR_MINOR}")
 
-         # Output base version found for logging in JAMF
+        # Output base version found for logging in JAMF
         echo "Base Version: ${appBaseVersion}"
 
         # Make sure we have data in both variables
         if [[ -n "${appSAPcode}" && -n "${appBaseVersion}" ]] ; then
 
             # Run the Adobe uninstall command
-            /usr/local/adobe/AdobeUninstaller --products="${appSAPcode}"#"${appBaseVersion}"
+            /usr/local/adobe/AdobeUninstaller --products="${appSAPcode}#${appBaseVersion}"
 
             # Verify app is removed
-            if [ -d "${appPath}" ]; then
-                # Uninstall failed
+            if [[ -d "${appPath}" ]]; then
                 echo "Application is still present. Uninstall failed"
             else
                 echo "Uninstall successful"
             fi
         else
-            # Stop here, we do not have both variables. Something happened.
             echo "Missing variable. SAP Code: ${appSAPcode} or Base Version: ${appBaseVersion}"
             exit 1
         fi
     else
-        # Echo not present
         echo "Product ${appPath} was not present"
     fi
-
 }
 
 appInstall() {
     # Check to see if the Application is still present
-    if [ ! -d "${appPath}" ]; then
+    if [[ ! -d "${appPath}" ]]; then
         
         echo "Event ID: ${appEventID}"
-    	echo "Product Path: ${appPath}"
+        echo "Product Path: ${appPath}"
         echo "Installing..."
         echo ""
        
-       	# Application is missing, let's reinstall
+        # Application is missing, let's reinstall
         /usr/local/bin/jamf policy -event ${appEventID}
 
         # Recheck path for successful install
-         if [ ! -d "${appPath}" ]; then
+        if [[ ! -d "${appPath}" ]]; then
             echo "Application failed to install"
         else
             echo "Application installed successfully"
         fi
     else
-        # Application path already present
-        echo "Application ${appChoosen} already installed"
+        echo "Application ${appSelected} already installed"
     fi
 }
 
@@ -214,20 +236,18 @@ appInstall() {
 
 ## Checks and balances ##
 
-## Stop here if no applications listed
-if [ -z "${appList}" ]; then
+# Check array length instead of just the string to handle Jamf arguments better
+if (( ${#appList[@]} == 0 )); then
     echo "Application list is missing. End of line"
     exit 1
 fi
 
-## Stop here if no action defined
-if [ -z "${actionVar}" ]; then
+if [[ -z "${actionVar}" ]]; then
     echo "Action variable not set. Stopping"
     exit 1
 fi
 
-## Stop here if no year defined
-if [ -z "${appYear}" ]; then
+if [[ -z "${appYear}" ]]; then
     echo "Application year not set. Stopping"
     exit 1
 fi
@@ -235,30 +255,25 @@ fi
 ## Action Items below ##
 
 ## Handle if all apps should be worked with
-# Get app list item count
 appListCount=${#appList[@]}
-if [ "${appListCount}" == 1 ]; then
-    ## See if count for app list is 1 and if so see if set to all
-    if [ "${appList[0]}" == "all" ]; then
+if [[ ${appListCount} -eq 1 ]]; then
+    ## Zsh arrays are 1-indexed (unlike Bash which is 0-indexed)
+    if [[ "${appList[1]}" == "all" ]]; then
         # If app list is set to just word all, fill in all apps here
         appList=("acrobat" "aftereffects" "animate" "audition" "bridge" "character" "dimension" "dreamweaver" "illustrator" "incopy" "indesign" "lightroomclassic" "mediaencoder" "photoshop" "premierepro" "premiererush")
     fi
 fi
 
-
 ## Determine what is in variable 4
-
 case ${actionVar} in
     "Uninstall")
-        for i in "${appList[@]}"
-        do
+        for i in "${appList[@]}"; do
             appSelector "${i}"
             appUninstall
         done
         ;;
     "Install")
-        for i in "${appList[@]}"
-        do
+        for i in "${appList[@]}"; do
             appSelector "${i}"
             appInstall
         done
